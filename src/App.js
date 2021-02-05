44701c683dece523d1ad7ed9ecfa460588dc8ef9
Kin <winterheat@gmail.com>
2021-02-05 06:48:44 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import { FaStar } from 'react-icons/fa';
 import Star from './Star';
 import StarRating from "./StarRating";
 import { useState, useEffect } from "react";
 
-// Demo 1: https://codesandbox.io/s/affectionate-brook-64gxw
+// Demo 1: https://codesandbox.io/s/epic-edison-6kf9i
+// Demo 2: a different behavior
+//           https://codesandbox.io/s/affectionate-brook-64gxw
 
 import './App.css';
 
